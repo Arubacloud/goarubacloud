09fc90d991a7cc66fd1e6589746cc71c0ee57656
Alessio Rocchi <rocchi.b.a@gmail.com>
2016-12-20 15:36:08 +0100

    Non working new template model

--- goarubacloud.go
+++ goarubacloud.go
@@ -10,7 +10,6 @@ type API struct {
 	client *Client
 }
 
-
 type SetEnqueueServerCreation struct {
 	ApplicationId string `json:"ApplicationId"`
 	RequestId     string `json:"RequestId"`
@@ -26,57 +25,57 @@ type SetEnqueueServerCreation struct {
 }
 
 type GetServerDetailsRequest struct {
-	ApplicationId	string	`json:"ApplicationId,omitempty"`
-	RequestId	string	`json:"RequestId,omitempty"`
-	Username	string	`json:"Username,omitempty"`
-	Password	string	`json:"Password,omitempty"`
-	ServerId	int	`json:"ServerId,omitempty"`
+	ApplicationId string        `json:"ApplicationId,omitempty"`
+	RequestId     string        `json:"RequestId,omitempty"`
+	Username      string        `json:"Username,omitempty"`
+	Password      string        `json:"Password,omitempty"`
+	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerDeletion struct {
-	ApplicationId	string	`json:"ApplicationId,omitempty"`
-	RequestId	string	`json:"RequestId,omitempty"`
-	Username	string	`json:"Username,omitempty"`
-	Password	string	`json:"Password,omitempty"`
-	ServerId	int	`json:"ServerId,omitempty"`
+	ApplicationId string        `json:"ApplicationId,omitempty"`
+	RequestId     string        `json:"RequestId,omitempty"`
+	Username      string        `json:"Username,omitempty"`
+	Password      string        `json:"Password,omitempty"`
+	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type Response struct {
-	Success		bool	`json:"Success,omitempty"`
-	Value		string	`json:"Value,omitempty"`
+	Success bool        `json:"Success,omitempty"`
+	Value   string        `json:"Value,omitempty"`
 }
 
 type SetEnqueueServerStart struct {
-	ApplicationId	string	`json:"ApplicationId,omitempty"`
-	RequestId	string	`json:"RequestId,omitempty"`
-	Username	string	`json:"Username,omitempty"`
-	Password	string	`json:"Password,omitempty"`
-	ServerId	int	`json:"ServerId,omitempty"`
+	ApplicationId string        `json:"ApplicationId,omitempty"`
+	RequestId     string        `json:"RequestId,omitempty"`
+	Username      string        `json:"Username,omitempty"`
+	Password      string        `json:"Password,omitempty"`
+	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerStop struct {
-	ApplicationId	string	`json:"ApplicationId,omitempty"`
-	RequestId	string	`json:"RequestId,omitempty"`
-	Username	string	`json:"Username,omitempty"`
-	Password	string	`json:"Password,omitempty"`
-	ServerId	int	`json:"ServerId,omitempty"`
+	ApplicationId string        `json:"ApplicationId,omitempty"`
+	RequestId     string        `json:"RequestId,omitempty"`
+	Username      string        `json:"Username,omitempty"`
+	Password      string        `json:"Password,omitempty"`
+	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type GetServersRequest struct {
-	ApplicationId	string	`json:"ApplicationId"`
-	RequestId	string  `json:"RequestId"`
-	Username	string	`json:"Username"`
-	Password	string	`json:"Password"`
+	ApplicationId string        `json:"ApplicationId"`
+	RequestId     string  `json:"RequestId"`
+	Username      string        `json:"Username"`
+	Password      string        `json:"Password"`
 }
 
 type GetTemplatesRequest struct {
-	ApplicationId	string	`json:"ApplicationId"`
-	RequestId	string	`json:"RequestId"`
-	Username	string	`json:"Username"`
-	Password	string	`json:"Password"`
+	ApplicationId string        `json:"ApplicationId"`
+	RequestId     string        `json:"RequestId"`
+	Username      string        `json:"Username"`
+	Password      string        `json:"Password"`
 }
 
-func NewAPI(endpoint, username, password string) (api * API, err error) {
+func NewAPI(endpoint, username, password string) (api *API, err error) {
 	client, err := NewClient(endpoint, username, password)
 	if err != nil {
 		return nil, err
@@ -84,18 +83,18 @@ func NewAPI(endpoint, username, password string) (api * API, err error) {
 	return &API{client}, nil
 }
 
-func (a *API) GetTemplates() (templates []*models.Template, err error) {
+func (a *API) GetTemplates() (hypervisorTypes []*models.GetHypvervisorTypeResponse, err error) {
 	var getTemplatesRequest = GetTemplatesRequest{}
 	getTemplatesRequest.Username = a.client.Username
 	getTemplatesRequest.Password = a.client.Password
 	getTemplatesRequest.ApplicationId = "GetHypervisor"
 	getTemplatesRequest.RequestId = "GetHypervisor"
-	err = a.client.Post("/GetHypervisors", getTemplatesRequest, &templates)
+	err = a.client.Post("/GetHypervisors", getTemplatesRequest, &hypervisorTypes)
 	if err != nil {
 		return nil, err
 	}
 
-	return templates, nil
+	return hypervisorTypes, nil
 }
 
 func (a *API) GetTemplate(templateId int) (template *models.Template, err error) {
@@ -103,16 +102,18 @@ func (a *API) GetTemplate(templateId int) (template *models.Template, err error)
 	if err != nil {
 		return nil, err
 	}
-
-	for _, template := range templates {
-		if template.Id == templateId {
-			return template, nil
+	for _, hv := range templates {
+		if hv.HypervisorType == 4 {
+			for _, template := range hv.Templates {
+				if template.Id == templateId {
+					return &template, nil
+				}
+			}
 		}
-
 	}
 
 	return nil,
-	fmt.Errorf("No template with ID: %d found on datacenter: %s", templateId, a.client.EndPoint)
+		fmt.Errorf("No template with ID: %d found on datacenter: %s", templateId, a.client.EndPoint)
 }
 
 func (a *API) GetServers() (servers []*models.Server, err error) {
