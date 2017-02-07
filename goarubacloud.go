168e78ac2a455c04c844ab80b2d652153c987fbc
ne29111984 <nicola.eusebi@gmail.com>
2017-02-07 16:56:19 +0100

    bugfix model and call ArubaCloud API

--- goarubacloud.go
+++ goarubacloud.go
@@ -11,8 +11,6 @@ type API struct {
 }
 
 type SetEnqueueServerCreation struct {
-	ApplicationId string `json:"ApplicationId"`
-	RequestId     string `json:"RequestId"`
 	Username      string `json:"Username"`
 	Password      string `json:"Password"`
 	Server        struct {
@@ -25,16 +23,12 @@ type SetEnqueueServerCreation struct {
 }
 
 type GetServerDetailsRequest struct {
-	ApplicationId string        `json:"ApplicationId,omitempty"`
-	RequestId     string        `json:"RequestId,omitempty"`
 	Username      string        `json:"Username,omitempty"`
 	Password      string        `json:"Password,omitempty"`
 	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerDeletion struct {
-	ApplicationId string        `json:"ApplicationId,omitempty"`
-	RequestId     string        `json:"RequestId,omitempty"`
 	Username      string        `json:"Username,omitempty"`
 	Password      string        `json:"Password,omitempty"`
 	ServerId      int        `json:"ServerId,omitempty"`
@@ -46,33 +40,31 @@ type Response struct {
 }
 
 type SetEnqueueServerStart struct {
-	ApplicationId string        `json:"ApplicationId,omitempty"`
-	RequestId     string        `json:"RequestId,omitempty"`
 	Username      string        `json:"Username,omitempty"`
 	Password      string        `json:"Password,omitempty"`
 	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerStop struct {
-	ApplicationId string        `json:"ApplicationId,omitempty"`
-	RequestId     string        `json:"RequestId,omitempty"`
 	Username      string        `json:"Username,omitempty"`
 	Password      string        `json:"Password,omitempty"`
 	ServerId      int        `json:"ServerId,omitempty"`
 }
 
 type GetServersRequest struct {
-	ApplicationId string        `json:"ApplicationId"`
-	RequestId     string  `json:"RequestId"`
 	Username      string        `json:"Username"`
 	Password      string        `json:"Password"`
 }
 
 type GetTemplatesRequest struct {
-	ApplicationId string        `json:"ApplicationId"`
-	RequestId     string        `json:"RequestId"`
 	Username      string        `json:"Username"`
 	Password      string        `json:"Password"`
+}
+
+type GetPreconfiguredPackagesRequest struct {
+	Username      string        `json:"Username"`
+	Password      string        `json:"Password"`
+	HypervisorType int 			`json:"HypervisorType"`
 }
 
 func NewAPI(endpoint, username, password string) (api *API, err error) {
@@ -87,8 +79,6 @@ func (a *API) GetTemplates() (hypervisorTypes []*models.GetHypvervisorTypeRespon
 	var getTemplatesRequest = GetTemplatesRequest{}
 	getTemplatesRequest.Username = a.client.Username
 	getTemplatesRequest.Password = a.client.Password
-	getTemplatesRequest.ApplicationId = "GetHypervisor"
-	getTemplatesRequest.RequestId = "GetHypervisor"
 	err = a.client.Post("/GetHypervisors", getTemplatesRequest, &hypervisorTypes)
 	if err != nil {
 		return nil, err
@@ -97,7 +87,38 @@ func (a *API) GetTemplates() (hypervisorTypes []*models.GetHypvervisorTypeRespon
 	return hypervisorTypes, nil
 }
 
-func (a *API) GetTemplate(templateId int) (template *models.Template, err error) {
+func (a *API) GetPreconfiguredPackages() (packagesTypes []*models.CloudPackage, err error) {
+	var getpackageRequest = GetPreconfiguredPackagesRequest{}
+	getpackageRequest.Username = a.client.Username
+	getpackageRequest.Password = a.client.Password
+	getpackageRequest.HypervisorType = 4
+	err = a.client.Post("/GetPreconfiguredPackages", getpackageRequest, &packagesTypes)
+	if err != nil {
+		return nil, err
+	}
+
+	return packagesTypes, nil
+}
+
+func (a *API) GetPreconfiguredPackage(packagename string) (cloudpackage *models.CloudPackage, err error) {
+	cloudpackages, err := a.GetPreconfiguredPackages()
+	if err != nil {
+		return nil, err
+	}
+	for _, cloudpackage := range cloudpackages {
+			for _, description := range cloudpackage.Descriptions {
+				if description.LanguageID == 2 && description.Text == packagename {
+					return cloudpackage, nil
+				}
+			}
+		
+	}
+
+	return nil,
+		fmt.Errorf("No package found with Name: %s found on datacenter: %s", packagename, a.client.EndPoint)
+}
+
+func (a *API) GetTemplate(templatename string) (template *models.Template, err error) {
 	templates, err := a.GetTemplates()
 	if err != nil {
 		return nil, err
@@ -105,7 +126,7 @@ func (a *API) GetTemplate(templateId int) (template *models.Template, err error)
 	for _, hv := range templates {
 		if hv.HypervisorType == 4 {
 			for _, template := range hv.Templates {
-				if template.Id == templateId {
+				if template.Name == templatename {
 					return &template, nil
 				}
 			}
@@ -113,15 +134,15 @@ func (a *API) GetTemplate(templateId int) (template *models.Template, err error)
 	}
 
 	return nil,
-		fmt.Errorf("No template with ID: %d found on datacenter: %s", templateId, a.client.EndPoint)
+		fmt.Errorf("No template with Name: %s found on datacenter: %s", templatename, a.client.EndPoint)
 }
+
+
 
 func (a *API) GetServers() (servers []*models.Server, err error) {
 	var getServersRequest = GetServersRequest{}
 	getServersRequest.Username = a.client.Username
 	getServersRequest.Password = a.client.Password
-	getServersRequest.ApplicationId = "GetServers"
-	getServersRequest.RequestId = "GetServers"
 	err = a.client.Post("/GetServers", getServersRequest, &servers)
 	if err != nil {
 		return nil, err
@@ -134,8 +155,6 @@ func (a *API) GetServer(serverId int) (server *models.Server, err error) {
 	var getServerDetailsRequest = GetServerDetailsRequest{}
 	getServerDetailsRequest.Username = a.client.Username
 	getServerDetailsRequest.Password = a.client.Password
-	getServerDetailsRequest.ApplicationId = "GetServerDetailsRequest"
-	getServerDetailsRequest.RequestId = "GetServerDetailsRequest"
 	getServerDetailsRequest.ServerId = serverId
 
 	err = a.client.Post("/GetServerDetails", getServerDetailsRequest, &server)
