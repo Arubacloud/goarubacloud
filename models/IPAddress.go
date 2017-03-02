ba00c68a4fb0644b97114f5a5c20355dc7af0600
ne29111984 <nicola.eusebi@gmail.com>
2017-03-02 12:11:55 +0100

    added model files

---
+++ models/IPAddress.go
@@ -0,0 +1,8 @@
+package models
+
+type IPAddress struct {
+	Value                          string        `json:"Value,omitempty"`
+	ResourceId                     int        	 `json:"ResourceId,omitempty"`
+}
+
+
