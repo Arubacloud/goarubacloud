86fbdb2920650c210192b2efb72c725f73464150
Mattias Cibien <mattias.cibien@outlook.com>
2017-03-09 09:16:08 +0100

    Added IPv6 support

--- goarubacloud.go
+++ goarubacloud.go
@@ -1,9 +1,10 @@
 package goarubacloud
 
 import (
+	"fmt"
+
 	"github.com/arubacloud/goarubacloud/models"
 	"github.com/docker/machine/libmachine/log"
-	"fmt"
 )
 
 type API struct {
@@ -11,97 +12,98 @@ type API struct {
 }
 
 type SetEnqueueServerCreation struct {
-	Username      string `json:"Username"`
-	Password      string `json:"Password"`
-	Server        struct {
-			      AdministratorPassword 	string `json:"AdministratorPassword"`
-			      Name                  	string `json:"Name"`
-			      SmartVMWarePackageID  	int    `json:"SmartVMWarePackageID,omitempty"`
-			      Note                  	string `json:"Note"`
-			      OSTemplateId          	int    `json:"OSTemplateId"`
-			      SshKey                	string `json:"SshKey"`
-			      SshPasswordAuthAllowed 	bool        `json:"SshPasswordAuthAllowed,omitempty"`
-			      CPUQuantity          	int    `json:"CPUQuantity"`
-			      RAMQuantity          	int    `json:"RAMQuantity"`
-			      NetworkAdaptersConfiguration []NetworkAdapterConfiguration
-			      VirtualDisks []VirtualDiskDetails
-		      }
+	Username string `json:"Username"`
+	Password string `json:"Password"`
+	Server   struct {
+		AdministratorPassword        string `json:"AdministratorPassword"`
+		Name                         string `json:"Name"`
+		SmartVMWarePackageID         int    `json:"SmartVMWarePackageID,omitempty"`
+		Note                         string `json:"Note"`
+		OSTemplateId                 int    `json:"OSTemplateId"`
+		SshKey                       string `json:"SshKey"`
+		SshPasswordAuthAllowed       bool   `json:"SshPasswordAuthAllowed,omitempty"`
+		CPUQuantity                  int    `json:"CPUQuantity"`
+		RAMQuantity                  int    `json:"RAMQuantity"`
+		NetworkAdaptersConfiguration []NetworkAdapterConfiguration
+		VirtualDisks                 []VirtualDiskDetails
+		ConfigureIPv6                bool `json:"ConfigureIPv6"`
+	}
 }
 
-type NetworkAdapterConfiguration struct{
-	PublicIpAddresses []PublicIpAddressDetails
-	NetworkAdapterType                  	int `json:"NetworkAdapterType"`		      	
+type NetworkAdapterConfiguration struct {
+	PublicIpAddresses  []PublicIpAddressDetails
+	NetworkAdapterType int `json:"NetworkAdapterType"`
 }
 
-type PublicIpAddressDetails struct{
-	PrimaryIPAddress 	bool        `json:"PrimaryIPAddress"`		
-	PublicIpAddressResourceId 	int        `json:"PublicIpAddressResourceId"`	      	
+type PublicIpAddressDetails struct {
+	PrimaryIPAddress          bool `json:"PrimaryIPAddress"`
+	PublicIpAddressResourceId int  `json:"PublicIpAddressResourceId"`
 }
 
-type VirtualDiskDetails struct{
-	Size 	int        `json:"Size,omitempty"`		
-	VirtualDiskType 	int        `json:"VirtualDiskType"`	      	
+type VirtualDiskDetails struct {
+	Size            int `json:"Size,omitempty"`
+	VirtualDiskType int `json:"VirtualDiskType"`
 }
 
 type GetServerDetailsRequest struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
-	ServerId      int        `json:"ServerId,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
+	ServerId int    `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerDeletion struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
-	ServerId      int        `json:"ServerId,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
+	ServerId int    `json:"ServerId,omitempty"`
 }
 
 type Response struct {
-	Success bool        `json:"Success,omitempty"`
-	Value   string        `json:"Value,omitempty"`
+	Success bool   `json:"Success,omitempty"`
+	Value   string `json:"Value,omitempty"`
 }
 
 type SetEnqueueServerStart struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
-	ServerId      int        `json:"ServerId,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
+	ServerId int    `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerStop struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
-	ServerId      int        `json:"ServerId,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
+	ServerId int    `json:"ServerId,omitempty"`
 }
 
 type SetEnqueueServerKill struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
-	ServerId      int        `json:"ServerId,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
+	ServerId int    `json:"ServerId,omitempty"`
 }
 
 type SetPurchaseIpAddress struct {
-	Username      string        `json:"Username,omitempty"`
-	Password      string        `json:"Password,omitempty"`
+	Username string `json:"Username,omitempty"`
+	Password string `json:"Password,omitempty"`
 }
 
 type GetServersRequest struct {
-	Username      string        `json:"Username"`
-	Password      string        `json:"Password"`
+	Username string `json:"Username"`
+	Password string `json:"Password"`
 }
 
 type GetTemplatesRequest struct {
-	Username      string        `json:"Username"`
-	Password      string        `json:"Password"`
+	Username string `json:"Username"`
+	Password string `json:"Password"`
 }
 
 type GetPreconfiguredPackagesRequest struct {
-	Username      string        `json:"Username"`
-	Password      string        `json:"Password"`
-	HypervisorType int 			`json:"HypervisorType"`
+	Username       string `json:"Username"`
+	Password       string `json:"Password"`
+	HypervisorType int    `json:"HypervisorType"`
 }
 
 type GetPurchasedIpAddresses struct {
-	Username      string        `json:"Username"`
-	Password      string        `json:"Password"`
+	Username string `json:"Username"`
+	Password string `json:"Password"`
 }
 
 func NewAPI(endpoint, username, password string) (api *API, err error) {
@@ -143,12 +145,12 @@ func (a *API) GetPreconfiguredPackage(packagename string) (cloudpackage *models.
 		return nil, err
 	}
 	for _, cloudpackage := range cloudpackages {
-			for _, description := range cloudpackage.Descriptions {
-				if description.LanguageID == 2 && description.Text == packagename {
-					return cloudpackage, nil
-				}
+		for _, description := range cloudpackage.Descriptions {
+			if description.LanguageID == 2 && description.Text == packagename {
+				return cloudpackage, nil
 			}
-		
+		}
+
 	}
 
 	return nil,
@@ -173,9 +175,9 @@ func (a *API) GetPurchasedIpAddress(ip string) (IpAddress *models.IPAddress, err
 		return nil, err
 	}
 	for _, IpAddress := range IPAddresses {
-				if IpAddress.Value == ip{
-					return IpAddress, nil
-				}
+		if IpAddress.Value == ip {
+			return IpAddress, nil
+		}
 	}
 
 	return nil,
@@ -190,7 +192,7 @@ func (a *API) GetTemplate(templatename string, hypervisorType int) (template *mo
 	for _, hv := range templates {
 		if hv.HypervisorType == hypervisorType {
 			for _, template := range hv.Templates {
-				if template.Name == templatename  && template.TemplateSellingStatus == 1 /*OnSale*/ {
+				if template.Name == templatename && template.TemplateSellingStatus == 1 /*OnSale*/ {
 					return &template, nil
 				}
 			}
@@ -200,8 +202,6 @@ func (a *API) GetTemplate(templatename string, hypervisorType int) (template *mo
 	return nil,
 		fmt.Errorf("No template with Name: %s found on datacenter: %s", templatename, a.client.EndPoint)
 }
-
-
 
 func (a *API) GetServers() (servers []*models.Server, err error) {
 	var getServersRequest = GetServersRequest{}
@@ -229,7 +229,7 @@ func (a *API) GetServer(serverId int) (server *models.Server, err error) {
 	return server, nil
 }
 
-func (a *API) CreateServerSmart(name, admin_password string, package_id, os_template_id int, sshKey string) (server *models.Server, err error) {
+func (a *API) CreateServerSmart(name, admin_password string, package_id, os_template_id int, sshKey string, configureIPv6 bool) (server *models.Server, err error) {
 	var createRequest SetEnqueueServerCreation
 	createRequest.Username = a.client.Username
 	createRequest.Password = a.client.Password
@@ -237,6 +237,7 @@ func (a *API) CreateServerSmart(name, admin_password string, package_id, os_temp
 	createRequest.Server.OSTemplateId = os_template_id
 	createRequest.Server.SmartVMWarePackageID = package_id
 	createRequest.Server.SshKey = sshKey
+	createRequest.Server.ConfigureIPv6 = configureIPv6
 	if len(admin_password) > 0 {
 		createRequest.Server.AdministratorPassword = admin_password
 		createRequest.Server.SshPasswordAuthAllowed = true
@@ -253,25 +254,24 @@ func (a *API) CreateServerSmart(name, admin_password string, package_id, os_temp
 	return server, nil
 }
 
-func (a *API) CreateServerPro(name, admin_password string, os_template_id int, sshKey string, ipID int, diskSize int, cpuQuantity int, ramQuantity int) (server *models.Server, err error) {
+func (a *API) CreateServerPro(name, admin_password string, os_template_id int, sshKey string, ipID int, diskSize int, cpuQuantity int, ramQuantity int, configureIPv6 bool) (server *models.Server, err error) {
 	var createRequest SetEnqueueServerCreation
 	createRequest.Username = a.client.Username
 	createRequest.Password = a.client.Password
 	createRequest.Server.Name = name
 	createRequest.Server.OSTemplateId = os_template_id
 	createRequest.Server.SshKey = sshKey
-	createRequest.Server.NetworkAdaptersConfiguration = make([]NetworkAdapterConfiguration,0)	
-	na := NetworkAdapterConfiguration{
-	}
+	createRequest.Server.NetworkAdaptersConfiguration = make([]NetworkAdapterConfiguration, 0)
+	createRequest.Server.ConfigureIPv6 = configureIPv6
+	na := NetworkAdapterConfiguration{}
 	na.NetworkAdapterType = 0
-	na.PublicIpAddresses = make([]PublicIpAddressDetails,0)
+	na.PublicIpAddresses = make([]PublicIpAddressDetails, 0)
 	publicIp := PublicIpAddressDetails{
-		PrimaryIPAddress : true,
-		PublicIpAddressResourceId : ipID,
+		PrimaryIPAddress:          true,
+		PublicIpAddressResourceId: ipID,
 	}
 	na.PublicIpAddresses = append(na.PublicIpAddresses, publicIp)
 	createRequest.Server.NetworkAdaptersConfiguration = append(createRequest.Server.NetworkAdaptersConfiguration, na)
-	
 
 	if len(admin_password) > 0 {
 		createRequest.Server.AdministratorPassword = admin_password
@@ -281,8 +281,8 @@ func (a *API) CreateServerPro(name, admin_password string, os_template_id int, s
 	}
 	createRequest.Server.VirtualDisks = make([]VirtualDiskDetails, 0)
 	vd := VirtualDiskDetails{
-		Size : diskSize,
-		VirtualDiskType : 0,
+		Size:            diskSize,
+		VirtualDiskType: 0,
 	}
 	createRequest.Server.VirtualDisks = append(createRequest.Server.VirtualDisks, vd)
 	createRequest.Server.CPUQuantity = cpuQuantity
